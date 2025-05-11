cb7663f46bfd926ceb863493f287ff56f106579b
shahadot99999 <shahadot99999@gmail.com>
2025-05-11 07:13:37 +0600

    Nested Route concepts

--- src/main.jsx
+++ src/main.jsx
@@ -7,16 +7,27 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router";
+import Home from './components/Home/Home.jsx';
+import About from './components/About/About.jsx';
+import Contact from './components/Contact/Contact.jsx';
+
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    element: <Home></Home>,
+    children: [
+      {
+        path: '/about',
+        element:<About></About>
+      },
+      {
+        path: '/contact',
+        element: <Contact></Contact>
+      }
+    ]
   },
-  {
-    path: "/about",
-    element: <div>I am in the about page</div>,
-  },
+  
   {
     path: "/contact",
     element: <div>please contact with me</div>,
