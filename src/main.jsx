58dd0f7059efb1819e2e55a3fd9d2d490c4deebc
shahadot99999 <shahadot99999@gmail.com>
2025-05-11 06:43:36 +0600

    SPA, routing and react router setup

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World</div>,
+  },
+  {
+    path: "/about",
+    element: <div>I am in the about page</div>,
+  },
+  {
+    path: "/contact",
+    element: <div>please contact with me</div>,
+  }
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+    {/* <App /> */}
+  </StrictMode>,
+)
